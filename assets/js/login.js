838e1cf8341640358e7cd425d7c544f672299633
Larissa <larigithub@gmail.com>
2025-09-23 20:38:36 -0300

    Renderizar formulário de login com funcionalidades de submit e troca para signup

---
+++ assets/js/login.js
@@ -0,0 +1,86 @@
+(() => {
+  let formLogin = document.getElementById("form-login");
+
+  function rendLoginForm() {
+    formLogin.innerHTML = `
+            <form id="form" method="post">
+                <div class="flex-column">
+                    <label for="email">Email</label>
+                </div>
+        
+                <div class="input-group">
+                    <i class="bi bi-person"></i>
+                    <input class="input" type="text" id="sign-email" name="email" placeholder="Enter your Email" required>
+                </div>
+        
+        
+                <div class="flex-column">
+                    <label for="password">Password</label>
+                </div>
+        
+                <div class="input-group">
+                    <i class="bi bi-lock"></i>
+                    <input class="input" type="text" name="password" id="sign-password" placeholder="Enter your Password" required>
+        
+                    <button type="button" class="show-password">
+                        <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z">
+                            </path>
+                        </svg>
+                    </button>
+                </div>
+        
+                <div class="flex-row">
+                    <div class="check-box">
+                        <input type="checkbox" name="remeber" id="remeber">
+                        <label for="remeber">Remeber me</label>
+                    </div>
+        
+                    <span class="span">Forgot password?</span>
+                </div>
+        
+                <button class="btn-submit" type="submit" id="submit">Sing In</button>
+                <p class="p">Don't have an account? <span class="span" id="span-signup">Sign Up</span>
+        
+                </p>
+                <p class="p line">Or With</p>
+        
+                <div class="flex-row">
+                    <button class="btn google" type="button">
+                        <img width="48" height="48" src="https://img.icons8.com/fluency/48/google-logo.png" alt="google-logo" />
+                    </button>
+        
+                    <button class="btn pinterest" type="button">
+                        <img width="48" height="48" src="https://img.icons8.com/color/48/pinterest--v1.png"
+                            alt="pinterest--v1" />
+                    </button>
+                </div>
+            </form> 
+            `;
+
+    const loginBtn = document.getElementById("btn-login");
+    const form = document.getElementById("form");
+    const spanSignUp = document.getElementById('span-signup')
+
+    spanSignUp.addEventListener('click', () => {
+        document.getElementById('form-login').style.display = "none";
+        document.getElementById('form-signup').style.display = "block";
+    });
+    
+    loginBtn.addEventListener("click", () => {
+      formLogin.classList.remove("hide");
+      formLogin.classList.add("show");
+    });
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const email = document.getElementById("sign-email").value;
+      const password = document.getElementById("sign-password").value;
+
+      console.log({ email, password });
+    });
+  }
+
+  rendLoginForm();
+})();
